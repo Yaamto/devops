f422da0f48785701fd21c993f9563b79dc71cd97
bilal <bilou728@hotmail.fr>
2024-02-12 15:17:51 +0100

    Post added

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
-
 # Charger les variables d'environnement à partir du fichier .env lorsque nous ne sommes pas en mode production
 if os.environ.get('ENV') != 'production':
     load_dotenv()
@@ -64,12 +63,13 @@ def get():
 # Route "/add" (POST) pour ajouter
 @app.route('/add', methods=['POST'])
 def create_task():
+
     if not request.json:
         return jsonify({'error': 'JSON expected'}), 400
-    interator = {
-        'state': request.json['state'],
-    }
 
+    interator = {
+        'state': request.json['value'],
+    }
     # BDD à faire pour créer
     return jsonify({'interator': interator}), 201
 
